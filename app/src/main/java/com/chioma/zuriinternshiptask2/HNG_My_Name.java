d3c788ea3300f2e52b07e539fc8c279a8f515f43
Chioma Ezeamama <58475309+Chiomsyn@users.noreply.github.com>
2021-08-19 05:17:51 +0100

    Create HNG_My_Name.java

---
+++ app/src/main/java/com/chioma/zuriinternshiptask2/HNG_My_Name.java
@@ -0,0 +1,12 @@
+package com.chioma.zuriinternshiptask2;
+
+public class HNG_My_Name {
+
+    public static void main(String[] args) {
+        
+        String name = "Ezeamama Chioma M.";
+        System.out.println("My name is: " + name);
+        
+    }
+    
+}
